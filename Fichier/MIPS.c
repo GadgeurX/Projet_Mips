79c051c3838c89d882ad6b59b8a0daf8c87d93d6
AlleeA <antoineallee@gmail.com>
2018-11-26 10:48:03 +0100

    ca marche pas mais c'est pour tester

--- Fichier/MIPS.c
+++ Fichier/MIPS.c
@@ -13,4 +13,16 @@
    printf("avec l'operande : %s\n", (*premier).Operande1);
    if((*premier).Operande2 != NULL){printf("Et : %s\n", (*premier).Operande2);}
    if((*premier).Operande3 != NULL){printf("Et : %s\n", (*premier).Operande3);}
+
+   premier = Decode (argv[1]);
+   printf("L'instruction est : %s\n", (*premier).Instruc);
+   printf("avec l'operande : %s\n", (*premier).Operande1);
+   if((*premier).Operande2 != NULL){printf("Et : %s\n", (*premier).Operande2);}
+   if((*premier).Operande3 != NULL){printf("Et : %s\n", (*premier).Operande3);}
+
+   premier = Decode (argv[1]);
+   printf("L'instruction est : %s\n", (*premier).Instruc);
+   printf("avec l'operande : %s\n", (*premier).Operande1);
+   if((*premier).Operande2 != NULL){printf("Et : %s\n", (*premier).Operande2);}
+   if((*premier).Operande3 != NULL){printf("Et : %s\n", (*premier).Operande3);}
 }
