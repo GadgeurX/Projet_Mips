b4cb71003dd8a27521fecf69dd5503b704174f6f
Lumosc <benoit.debief@free.fr>
2018-11-29 19:37:55 +0100

    Rectification et invertion, test pour tester et master pour la save

--- Fichier/charToHexa.h
+++ Fichier/charToHexa.h
@@ -5,6 +5,6 @@ void charToHexa( InstructionBrut instruction[]);//Fonction de redirection
 void convertionInstructionTypeJ (char* instruction, char* operande1);
 char* convertionInstructionTypeI (char* instruction, char* operande1, char* operande2);
 char* convertionInstructionTypeR (char* instruction, char* operande1, char* operande2, char* operande3);
-//
+
 
 #endif
