b3e1cb9776d2d51682f1226842b3efb392f56c97
Lumosc <benoit.debief@free.fr>
2018-12-02 11:00:14 +0100

    Fonction de convertion pour les instructions de type J OKAY, manque que de les transferer pour ecriture, sous format hexa. Homologation pour type I et R.

--- Fichier/test.c
+++ Fichier/test.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "Read.h"
+#include "charToHexa.h"
+
 int convAphaToDec(char* chaine, int i, int taille){
 
   printf("carac = %d\n", chaine[taille - i]);
@@ -33,7 +36,42 @@ void main(){
   int val = convAphaToDec(chaine, 1, taille);
 
   printf("%d\n",val );*/
+/*
+  char tab[3][3] = {{"A4"}, {"T1"}, {"VF"}};
+  char ** pointeur = NULL;
+  pointeur = &tab[1][2];
+  printf("%c\n", *pointeur);
+*/
 
-  printf("%x\n", 16 );
+  char sortie[9] = {"00011100"};
+  char rep[3] = {0};
+
+for(int i=0; i<2; i ++){
+
+  int j =0;
+  char tabTest[5] = {sortie[4*i], sortie[(4*i) + 1], sortie[(4*i) + 2], sortie[(4*i) + 3]};
+  printf("%s\n", tabTest);
+  char** pointeur = NULL;
+  printf("test\n" );
+  while(pointeur == NULL){//on suppose le bloc de caractere traite juste
+
+    printf("nfois\n" );
+    if(strcmp(binaireToHexa[2*j], tabTest) == 0){//meme chaine
+
+      pointeur = &binaireToHexa[(2*j)+1];//pointe vers l'entree du tableau correspondant à la traduction
+
+    }
+
+    j++;
+
+  }
+
+  printf("le caractere en hexa est:%c\n", *pointeur);
+
+  rep[i] = *pointeur;
+
+}
+
+printf("%s\n",rep );
 
 }
