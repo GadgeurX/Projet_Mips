3bdcc87b812d1b5b7238090e52c7d014858f922f
Niopsa <a42chevillard@gmail.com>
2018-12-05 12:44:00 +0100

    Creation des fonctions du processeur Mips

--- Fichier/src/fonctions.h
+++ Fichier/src/fonctions.h
@@ -2,6 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
+void SSL(int instruct);
+
+void NOP(int instruct);
+
+void ROTR(int instruct);
+
+void SRL(int instruct);
+
+void SYSCALL(int instruct);
+
+void MHFI(int instruct);
+
+void MHLO(int instruct);
+
+void MULT(int instruct);
+
+void DIV(int instruct);
+
+void ADD(int instruct);
+
+void SUB(int instruct);
+
+void AND(int instruct);
+
+void OR(int instruct);
+
+void XOR(int instruct);
+
 void jump(int instruct);
+
+void BEQ(int instruct);
+
+void BNE(int instruct);
+
+void BLEZ(int instruct);
+
+void BGTZ(int instruct);
+
+void ADDI(int instruct);
+
+void LUI(int instruct);
+
+void LW(int instruct);
+
+void SW(int instruct);
 
 void lireCodeHexa();
