923a1b2a88e6346bf70da9f0ad02ff3bee0f542d
Romain Allée <romain.allee@epitech.eu>
2018-12-22 23:16:22 +0100

    initialise_compiler

---
+++ include/binary_instruction.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#ifndef BINARY_INSTRUCTION_H_
+#define BINARY_INSTRUCTION_H_
+
+typedef struct binary_instruction_s {
+  uint8_t Instruc;
+  operand Operande1;
+  operand Operande2;
+  operand Operande3;
+} binary_instruction;
+
+typedef struct operand_s {
+  operand_type type;
+  uint8_t registerRef;
+  uint16_t immediate;
+  uint32_t index_offset;
+} operand;
+
+typedef enum operand_type_e { REGISTER, IMMEDIATE, INDEX } operand_type;
+
+#endif // READ_H_
--- include/compiler.h
+++ include/compiler.h
@@ -1,1 +1,6 @@
+#ifndef COMPILER_H_
+#define COMPILER_H_
+
 void compileFile(char* file);
+
+#endif COMPILER_H_
