ac80d293b164822130c1914a2094b1ee84321bbc
Romain Allée <romain.allee@epitech.eu>
2018-12-23 01:12:24 +0100

    reformate compile.c

--- src/compiler.c
+++ src/compiler.c
@@ -4,7 +4,8 @@
 #include "binary_instruction.h"
 #include "data_instruct.h"
 
-int get_instruction_count(raw_instruction* r_instruction) {
+int get_instruction_count(raw_instruction *r_instruction)
+{
     int count = 0;
     while (!r_instruction->instruc) {
         count++;
@@ -13,8 +14,9 @@ int get_instruction_count(raw_instruction* r_instruction) {
     return count;
 }
 
-match_instruct_hexa* get_instruction_from_string(char* instruction) {
-    match_instruct_hexa* tmp_matching = tab_instruct;
+match_instruct_hexa *get_instruction_from_string(char *instruction)
+{
+    match_instruct_hexa *tmp_matching = tab_instruct;
     while (tmp_matching->name != NULL) {
         if (strcmp(tmp_matching->name, instruction) == 0)
             return tmp_matching;
@@ -23,12 +25,23 @@ match_instruct_hexa* get_instruction_from_string(char* instruction) {
     return NULL;
 }
 
-void parse_operand(char* operand_name, operand* operand) {
-
+void parse_operand(char *operand_name, operand *operand)
+{
+    operand->type = NONE;
+    if (operand_name == NULL)
+        return;
+    if (strstr(operand_name, "$") == NULL) {
+        operand->type = IMMEDIATE;
+        operand->registerRef = atoi(operand_name);
+    } else {
+        operand->type = REGISTER;
+        operand->registerRef = atoi(operand_name + 1);
+    }
 }
 
-binary_instruction* process_raw_instruction(raw_instruction* r_instruction) {
-    binary_instruction* b_instruction = malloc(sizeof(binary_instruction) * get_instruction_count(r_instruction));
+binary_instruction *process_raw_instruction(raw_instruction *r_instruction)
+{
+    binary_instruction *b_instruction = malloc(sizeof(binary_instruction) * get_instruction_count(r_instruction));
     while (!r_instruction->instruc) {
         b_instruction->instruction = *get_instruction_from_string(r_instruction->instruc);
         if (r_instruction->operande2 == NULL && r_instruction->operande3 == NULL && r_instruction->operande1 != NULL) {
@@ -43,9 +56,12 @@ binary_instruction* process_raw_instruction(raw_instruction* r_instruction) {
     }
 }
 
-void compileFile(char* file) {
-    raw_instruction* r_instruction = parse(file);
-    binary_instruction* b_instruction = process_raw_instruction(r_instruction);
+void compileFile(char *file)
+{
+    raw_instruction *r_instruction = parse(file);
+    binary_instruction *b_instruction = process_raw_instruction(r_instruction);
+
+    // Fonction antoine 
 
     free(r_instruction);
     free(b_instruction);
