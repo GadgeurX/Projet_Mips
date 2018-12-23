aa6f62da5b7fa3b6d775c3b83e470b89e7fbc7d7
Romain Allée <romain.allee@epitech.eu>
2018-12-23 13:22:33 +0100

    fix some stuff

--- src/compiler.c
+++ src/compiler.c
@@ -3,12 +3,12 @@
 #include "parser.h"
 #include "binary_instruction.h"
 #include "data_instruct.h"
+#include "data_register.h"
 
 /**
- * 
+ * Get the lenght of raw_instruction array
  */
-int get_instruction_count(raw_instruction *r_instruction)
-{
+int get_instruction_count(raw_instruction *r_instruction) {
     int count = 0;
     while (!r_instruction->instruction) {
         count++;
@@ -18,10 +18,9 @@ int get_instruction_count(raw_instruction *r_instruction)
 }
 
 /**
- * 
+ * Try to return a match_instruct_hexa type from instruction string in tab_instruct
  */
-match_instruct_hexa *get_instruction_from_string(char *instruction)
-{
+match_instruct_hexa *get_instruction_from_string(char *instruction) {
     match_instruct_hexa *tmp_matching = tab_instruct;
     while (tmp_matching->name != NULL) {
         if (strcmp(tmp_matching->name, instruction) == 0)
@@ -32,33 +31,49 @@ match_instruct_hexa *get_instruction_from_string(char *instruction)
 }
 
 /**
- * 
+ * Try to return a match_instruct_hexa type from instruction string in tab_instruct
  */
-void parse_operand(char *operand_name, operand *operand)
-{
+register_data *get_register_from_string(char *register) {
+    register_data *tmp_matching = register_data;
+    while (tmp_matching->name != NULL) {
+        if (strcmp(tmp_matching->name, instruction) == 0)
+            return tmp_matching;
+        tmp_matching++;
+    }
+    return NULL;
+}
+
+/**
+ * Try to convert imediate and register operand to a real value
+ */
+void parse_operand(char *operand_name, operand *operand) {
     operand->type = NONE;
     if (operand_name == NULL)
         return;
     if (strstr(operand_name, "$") == NULL) {
         operand->type = IMMEDIATE;
-        operand->registerRef = atoi(operand_name);
+        operand->value.immediate = atoi(operand_name);
     } else {
         operand->type = REGISTER;
-        operand->registerRef = atoi(operand_name + 1);
+        if (isdigit(*(operand_name + 1)))
+            operand->value.registerRef = atoi(operand_name + 1);
+        else
+            operand->value.registerRef = register_value
     }
 }
 
 /**
+ * Convert raw instruction to binary insruction.
  * 
+ * I and J type are handle with the same function
  */
-binary_instruction *process_raw_instruction(raw_instruction *r_instruction)
-{
+binary_instruction *process_raw_instruction(raw_instruction *r_instruction) {
     binary_instruction *b_instruction = malloc(sizeof(binary_instruction) * get_instruction_count(r_instruction));
     while (!r_instruction->instruction) {
         b_instruction->instruction = *get_instruction_from_string(r_instruction->instruction);
         if (r_instruction->operand2 == NULL && r_instruction->operand3 == NULL && r_instruction->operand1 != NULL) {
             b_instruction->operand1.type = INDEX;
-            b_instruction->operand1.index_offset = atoi(r_instruction->operand1);
+            b_instruction->operand1.value.index_offset = atoi(r_instruction->operand1);
         } else {
             parse_operand(r_instruction->operand1, &(b_instruction->operand1));
             parse_operand(r_instruction->operand2, &(b_instruction->operand2));
@@ -69,10 +84,10 @@ binary_instruction *process_raw_instruction(raw_instruction *r_instruction)
 }
 
 /**
- * Main fonction of the compiler
+ * Main fonction of the compiler.
+ * 
  */
-void compileFile(char *file)
-{
+void compileFile(char *file) {
     raw_instruction *r_instruction = parse(file);
     binary_instruction *b_instruction = process_raw_instruction(r_instruction);
 
