48b0d6cea3a502ef6b7ae14ce1d1f696884f441d
AlleeA <antoineallee@gmail.com>
2018-12-23 14:00:16 +0100

    plus de warning

--- src/stick_instruct.c
+++ src/stick_instruct.c
@@ -13,14 +13,14 @@ uint32_t* stick(binary_instruction *instruct){
           hexa_instruct = 0;
       }
       else if ((instruct + i)->instruction.type == I){
-          hexa_instruct = (instruct + i)->instruction.instruc << 26;
-          hexa_instruct += (instruct + i)->operand1.value.registerRef << 21;
-          hexa_instruct += (instruct + i)->operand2.value.registerRef << 16;
-          hexa_instruct += (instruct + i)->operand3.value.immediate;
+          *(hexa_instruct + i) = (instruct + i)->instruction.instruc << 26;
+          *(hexa_instruct + i) += (instruct + i)->operand1.value.registerRef << 21;
+          *(hexa_instruct + i) += (instruct + i)->operand2.value.registerRef << 16;
+          *(hexa_instruct + i) += (instruct + i)->operand3.value.immediate;
       }
       else{
-        hexa_instruct = (instruct + i)->instruction.instruc << 26;
-        hexa_instruct += (instruct + i)->operand1.value.index_offset;
+        *(hexa_instruct + i) = (instruct + i)->instruction.instruc << 26;
+        *(hexa_instruct + i) += (instruct + i)->operand1.value.index_offset;
       }
   i++;
   }
